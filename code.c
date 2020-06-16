2ff9c55ed1f136f784ab825f638eea01f2951075
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-06-16 12:01:48 +0530

    Added Check status Module

--- code.c
+++ code.c
@@ -11,12 +11,12 @@ void cancellation();
 void status();
 int choice,count;
 char temp[100];
-int temp_seat,temp_age,i;
+int temp_seat,temp_age,i,flag=0;
 int main()
 {
 	while(1)
 	{
-		printf("\nWelcome to Ticket Booking/Cancellation E-Portal");
+		printf("\n\nWelcome to Ticket Booking/Cancellation E-Portal");
 		printf("\nThe Services are\n1. BOOKING\n2. CANCELLATION\n3. CHECK STATUS\n4. EXIT\nEnter your choice\t");
 		scanf("%d",&choice);
 		switch(choice)
@@ -30,19 +30,19 @@ int main()
 			{
 				cancellation();
 				break;
-			}/*
+			}
 			case 3:
 			{
 				status();
 				break;	
-			}*/
+			}
 			case 4:
 			{
 				exit(1);	
 			}
 			default:
 			{
-				printf("Enter the valid choice\n");
+				printf("\nEnter the valid choice\n");
 				break;	
 			}
 		}
@@ -98,3 +98,23 @@ void cancellation()
 		printf("\nPlease Enter the correct information\n");
 	}
 }
+void status()
+{
+	printf("Please Enter your Name to check the status\t");
+	scanf("%s",temp);
+	for(i=0;i<100;i++)
+	{
+		if(!strcmp(s[i].name,temp))
+		{
+			printf("Welcome %s You booked the ticket %d",temp,i);
+			flag=1;
+			break;	
+		}	
+		
+	}
+	if(flag==0)
+	{
+		printf("\nOops! You don't have any tickets. Please enter the correct name");	
+	}
+}
+
